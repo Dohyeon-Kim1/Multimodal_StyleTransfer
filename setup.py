e1dd88618991301997a55381f559bb5c297d4571
김도현 <dhkim@gimdohyeon-ui-MacBookAir.local>
2023-06-26 22:47:54 +0900

    2023.06.26

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="OBSTwT",
     version="0.1.0",
     description="Package for Object and Bacground Style Transfer with Text.",
-    url="https://github.com/Dohyeon-Kim1/Object-Background_StyleTransfer_withText.git",
+    packages=find_packages(exclude="notebooks")
     install_requires=[
         "diffusers",
         "transforms",
